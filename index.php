a48f1c4c64f3c607005faefeb637f37d22b765f9
shinichi <0503baku@gmail.com>
2020-09-03 20:05:06 +0900

    ペットショップアプリ課題2

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html>
 
+<head>
+    <meta charset="utf-8">
+    <title>pet_shop</title>
+</head>
+
+<body>
+    <h2>本日のご紹介ペット!</h2>
+    <form action="" method="get">
+        <div>
+            <label for="keyword">キーワード:</label>
+            <input type="text" name="keyword" placeholder="キーワードの入力">
+            <input type="submit" value="検索">
+        </div>
+        <br>
+    </form>
+</body>
+
+</html>
+
+<?php
 // 接続に必要な情報を定義
 define('DSN', 'mysql:host=db;dbname=pet_shop;charset=utf8;');
 define('USER', 'staff');
@@ -15,23 +36,43 @@ try {
     exit;
 }
 
-// SQL文の組み立て
-$sql = 'SELECT * FROM animals';
+if ($_GET === '') {
+    // SQL文の組み立て
+    $sql = 'SELECT * FROM animals';
 
-// プリペアドステートメントの準備
-// $dbh->query($sql) でも良い
-$stmt = $dbh->prepare($sql);
+    // プリペアドステートメントの準備
+    // $dbh->query($sql) でも良い
+    $stmt = $dbh->prepare($sql);
 
-// プリペアドステートメントの実行
-$stmt->execute();
+    // プリペアドステートメントの実行
+    $stmt->execute();
 
-// 結果の受け取り
-$animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // 結果の受け取り
+    $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} else {
 
-foreach ($animals as $animal) {
-    echo $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん<br>';
-    echo $animal['description'] . '<br>';
-    echo '出身地' . $animal['birthplace'] . '<br>';
-    echo $animal['birthday'] . '生まれ<br>';
-    echo '<hr>';
+    $keyword = $_GET['keyword'];
+    $keyword = '%' . $keyword . '%'; // %ではさむ
+    // SQL文の組み立て
+    $sql = 'SELECT * FROM animals WHERE description LIKE :keyword';
+    // プリペアドステートメントの準備
+    // $dbh->query($sql) でも良い
+    $stmt = $dbh->prepare($sql);
+
+    // プリペアドステートメントの実行
+    $stmt->bindValue(':keyword', $keyword, PDO::PARAM_STR);
+    $stmt->execute();
+
+    // 結果の受け取り
+    $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
+?>
+
+<?php foreach ($animals as $animal) : ?>
+    <?= $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん' ?><br>
+    <?= $animal['description'] ?><br>
+    <?= '出身地' . $animal['birthplace'] ?><br>
+    <?= $animal['birthday'] . '生まれ' ?>
+    <hr>
+<?php endforeach; ?>
